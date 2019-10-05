7b37fc6926dcc41611a5fe105de57af4bb673577
atmarc <marquitus99@gmail.com>
2019-10-05 21:35:35 +0200

    peticions Reddit comments funciona

--- pythonService/Reddit.py
+++ pythonService/Reddit.py
@@ -2,26 +2,29 @@ import praw
 import requests
 import requests.auth
 from Amazon import Amazon
+from Connection import Connection
+import json
 
 a = Amazon('en')
 
 class Reddit:
 
     def tractar_comment (self, comment, paraulesClau):
-        self.tractar_text(comment.author.name, text)
+        self.tractar_text(comment.author.name, comment.body)
 
     def tractar_text(self, author, text):
         descartar = True
-        
+        confidence = 0
         try:
             res = a.analyze(text)
             print('.')
-            if res['SentimentScore']['Negative'] > 0.95:
+            confidence = res['SentimentScore']['Negative'] 
+            if confidence > 0.95:
                 descartar = False
                 print(text)
                 print("------------------------------------")
         except:
-            print('************* Error fent la petició *************')
+            print('************* Error fent la petició a amazon *************')
 
         '''
         textSplit = text.split(' ')
@@ -31,7 +34,7 @@ class Reddit:
                     descartar = False
         '''
         
-         profileData = {
+        profileData = {
             "platform": "rd",
 	        "name": str(author),
 	        "link": "https://www.reddit.com/user/" + str(author)
@@ -45,37 +48,29 @@ class Reddit:
             "index": confidence
         }
         
-        if confidence > 0.95:
-            res = Connection().getProfile(str(author))
-            if len(res.text) == 2:      
-                profileData = json.dumps(profileData)
-                res = Connection().newProfile(profileData)
-                res = json.loads(res.text)
-                bullyData["user"]=res["id"]
-                bullyData = json.dumps(bullyData)
-                res = Connection().newBully(bullyData)
-            else:
-                res = json.loads(res.text)
-                bullyData["user"]=res["id"]
-                bullyData = json.dumps(bullyData)
-                res = Connection().newBully(bullyData)
-            print(res)
-
-    def tractar_post(self, post, paraulesClau):
-        print(post.title)
-        print('-------------------------------------')
-
+        try:
+            if not descartar:
+                res = Connection().getProfile(str(author))
+                if len(res.text) == 2:      
+                    profileData = json.dumps(profileData)
+                    res = Connection().newProfile(profileData)
+                    res = json.loads(res.text)
+                    bullyData["user"]=res["id"]
+                    bullyData = json.dumps(bullyData)
+                    res = Connection().newBully(bullyData)
+                else:
+                    res = json.loads(res.text)
+                    bullyData["user"]=res["id"]
+                    bullyData = json.dumps(bullyData)
+                    res = Connection().newBully(bullyData)
+                print(res)
+        except:
+            print('************* Error fent la petició a connection *************')
 
     def __init__(self, tipus, paraulesClau):
         r = praw.Reddit(client_id='sJ9uTn8JAn6M9A',
                      client_secret='zqATGmAb64i-nsyEEOQXhNPBa6o',
                      user_agent='python:mamuthack/1.0.0 by atmarc99')
 
-        if tipus == 'comments':
-            for comment in r.subreddit('all').stream.comments():
-                self.tractar_comment(comment, paraulesClau)
-        
-        if tipus == 'posts':
-            for submission in r.subreddit('all').stream.submissions():
-               self.tractar_post(submission, paraulesClau)
-
+        for comment in r.subreddit('all').stream.comments():
+            self.tractar_comment(comment, paraulesClau)
